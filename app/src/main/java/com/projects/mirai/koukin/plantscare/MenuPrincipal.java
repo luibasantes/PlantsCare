61b2f3cd5a5cbacb675c63d15b9d398e04b39f3d
luibasantes <luibasantes@hotmail.com>
2018-06-29 17:22:24 -0500

    validando que tenga internet

--- app/src/main/java/com/projects/mirai/koukin/plantscare/MenuPrincipal.java
+++ app/src/main/java/com/projects/mirai/koukin/plantscare/MenuPrincipal.java
@@ -1,7 +1,13 @@
 package com.projects.mirai.koukin.plantscare;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MenuPrincipal extends AppCompatActivity {
 
@@ -9,5 +15,45 @@ public class MenuPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+        if(!isConnected(MenuPrincipal.this)) buildDialog(MenuPrincipal.this).show();
+        else {
+            Toast.makeText(MenuPrincipal.this,"Bienvenido", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    public boolean isConnected(Context context) {
+
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netinfo = cm.getActiveNetworkInfo();
+
+        if (netinfo != null && netinfo.isConnectedOrConnecting()) {
+            android.net.NetworkInfo wifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+            android.net.NetworkInfo mobile = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+            if((mobile != null && mobile.isConnectedOrConnecting()) || (wifi != null && wifi.isConnectedOrConnecting())) return true;
+        else return false;
+        } else
+        return false;
+    }
+
+    public AlertDialog.Builder buildDialog(Context c) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(c);
+        //builder.setTitle("No Internet Connection");
+        //builder.setMessage("You need to have Mobile Data or wifi to access this. Press ok to Exit");
+        builder.setTitle("Sin Conexión al Internet");
+        builder.setMessage("Necesitas tener internet para usar el APP");
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+
+            }
+        });
+
+        return builder;
     }
 }
